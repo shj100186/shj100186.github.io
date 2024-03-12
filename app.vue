42448f232585c23e2f4b324424e9eb6be07a2143
shj100186 <46507376+shj100186@users.noreply.github.com>
2024-03-13 00:13:47 +0900

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,9 +1,12 @@
 <template>
-  <div v-if="isLoading">Loading...</div>
-  <div v-else-if="error">Error: {{ error.message }}</div>
+  <div v-if="error">Error: {{ error.message }}</div>
   <div v-else>
+    <h4>loading : {{ isLoading ? 'loading...' : 'done' }}</h4>
     <h3>{{ data?.title }}</h3>
     <p>ID: {{ data?.id }}</p>
+    <h4>loading2 : {{ isLoading2 ? 'loading...' : 'done' }}</h4>
+    <h3>{{ data2?.title }}</h3>
+    <p>ID2: {{ data2?.id }}</p>
     <button @click="onNext">next</button>
     <button @click="onPrev">prev</button>
     <button @click="onRefetch">refetch</button>
@@ -12,11 +15,19 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { useTodo } from './query'
+import { useTodo2 } from './query2';
 
 const todoId = ref(1);
-const { data, isLoading, error, refetch, remove, setID } = useTodo(todoId.value);
+const { data, error, isLoading, refetch, remove, setID } = useTodo(todoId.value);
+
+const { data: data2, isLoading: isLoading2, setEnabled } = useTodo2(100, false);
+
+watchEffect(() => {
+  const isValid = data.value?.id !== undefined
+  setEnabled(isValid)
+})
 
 const onNext = () => {  
   todoId.value += 1
